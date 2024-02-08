326771df3c5b4d75232b1a67611fabca1bfaad34
Gilvan-Saraiva <juniorsaraivabi@gmail.com>
2024-02-08 18:12:03 -0300

    testes json, novos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,32 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './template/header/header.component';
 import { RodapeComponent } from './template/rodape/rodape.component';
 import { PrincipalComponent } from './principal/principal.component';
+import { RestritoComponent } from './restrito/restrito.component';
+import { AtualizaProdutoComponent } from './restrito/atualiza-produto/atualiza-produto.component';
+import { CadastroProdutoComponent } from './restrito/cadastro-produto/cadastro-produto.component';
+import { ListaProdutoComponent } from './restrito/lista-produto/lista-produto.component';
+import { MenuProdutoComponent } from './restrito/menu-produto/menu-produto.component';
+
+// Importe os módulos necessários do Angular Material
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RodapeComponent,
-    PrincipalComponent
+    PrincipalComponent,
+    RestritoComponent,
+    AtualizaProdutoComponent,
+    CadastroProdutoComponent,
+    ListaProdutoComponent,
+    MenuProdutoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // Adicione os módulos do Angular Material aqui
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
